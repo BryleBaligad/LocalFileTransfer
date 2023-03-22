74c2192c6cc3f35f836fdcf15836f04fcc5d41ac
Bryle Baligad <brylematthewbaligad.fernandez@gmail.com>
2023-03-22 19:50:29 +0800

    Initial Commit

---
+++ lft.js
@@ -0,0 +1,89 @@
+const fs = require("fs")
+const http = require("http");
+const prompt = require("prompt-sync")();
+const formidable = require("formidable");
+const cliProgress = require("cli-progress")
+
+const PORT = process.env.PORT || 9876;
+const SAVEPATH = process.env.SAVEPATH || "./" ;
+
+http.createServer(async (req, res) => {
+    switch (req.url.split("|")[0]) {
+        case "/upload":
+            console.clear();
+            console.log("Incoming File Transfer!");
+            let acceptFile = await prompt(`Accept incoming file "${decodeURIComponent(req.url.split("|")[1])}" [Y/n] `);
+            if (acceptFile.toLowerCase() == "y" || acceptFile.toLowerCase() == "") {
+                let form = new formidable.IncomingForm({ maxFileSize: 1024 * 1024 * 1024});
+                var bar = new cliProgress.SingleBar({etaBuffer: 64, fps: 1, format: '[{bar}] {percentage}% | ETA: {eta}s | {value}/{total}'}, cliProgress.Presets.legacy)
+                bar.start(100, 0);
+                form.on("progress", (d, m) => {
+                    bar.setTotal(m)
+                    bar.update(d)
+                })
+                form.parse(req, async (err, fields, files) => {
+                    bar.stop();
+                    var oldpath = files.file.filepath;
+                    var newpath = SAVEPATH + files.file.originalFilename;
+                    fs.cp(oldpath, newpath, async err => {
+                        if (err) throw err;
+                        console.log(`File received! Saved to ${newpath}`)
+                        res.write("Uploaded");
+                        res.end();
+                    })
+                })
+            } else {
+                console.log("File rejected")
+                res.write("Upload rejected by host");
+                res.end();
+            }
+            break;
+
+        default:
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.write(html.replace("%versionData%", "LFT v0.0.1"))
+            res.end()
+    }
+}).listen(PORT)
+
+console.info("Running on port " + PORT)
+
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        * {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+    <title>Local File Upload</title>
+</head>
+<body>
+    <center>
+        <h1>Local File Transfer</h1>
+        Select a file below and click submit query to upload.
+        <br>
+        <br>
+        <form id="form" action="upload" method="post" enctype="multipart/form-data">
+            <input id="file" type="file" name="file" required>
+            <br>
+            <br>
+            <input type="submit">
+        </form>
+        <br>
+        <br>
+        %versionData%
+    </center>
+    <script>
+        document.getElementById("form").action = "upload|" + document.getElementById("file").files[0].name
+        document.getElementById("file").addEventListener("input", evt => {
+            document.getElementById("form").action = "upload?" + document.getElementById("file").files[0].name
+        })
+    </script>
+</body>
+</html>
+`
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Local File Transfer
+
+Because why not, here's a web-based file transfer doohickey.
